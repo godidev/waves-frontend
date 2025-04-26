5f1c973b947309c449b6b3582d4f5a311c0f342b
godidev <gdudev@proton.me>
2025-04-26 18:48:47 +0200

    feat: change buoy to its own page component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/HomePage'
 import { NotFoundPage } from './pages/NotFoundPage'
-import BuoyTable from './components/buoys/BuoyTable'
+import BuoyPage from './pages/BuoysPage'
 import SurfForecastPage from './pages/SurfForecastPage'
 
 const router = createBrowserRouter([
@@ -12,7 +12,7 @@ const router = createBrowserRouter([
     element: <Layout />,
     children: [
       { path: '/', element: <Home /> },
-      { path: '/buoys', element: <BuoyTable /> },
+      { path: '/buoys', element: <BuoyPage /> },
       { path: '/forecast', element: <SurfForecastPage /> },
     ],
   },
